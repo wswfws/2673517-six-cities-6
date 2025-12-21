3af34a5d3d5021688aa361b97ded59de10815386
wswfws <wswfws@ya.ru>
2025-12-21 13:03:56 +0500

    fix: tag <a /> to react router <Link />

--- src/components/widgets/footer.tsx
+++ src/components/widgets/footer.tsx
@@ -1,9 +1,11 @@
 import {Link} from 'react-router-dom';
+import {ROUTE_CONFIG} from "../app/use-app-routes.ts";
 
 export default function Footer() {
+
   return (
     <footer className='footer container'>
-      <Link className='footer__logo-link' to='/'>
+      <Link className='footer__logo-link' to={ROUTE_CONFIG.ROOT}>
         <img className='footer__logo' src='/img/logo.svg' alt='6 cities logo' width='64' height='33'/>
       </Link>
     </footer>
